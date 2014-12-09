8e5dff94be25a70287792121d1bdfc52409fcaea
fullgod <fullgod@gmail.com>
2014-12-09 16:03:52 +0300

    commit main

---
+++ header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width" />
+<title><?php wp_title( '|', true, 'right' ); ?></title>
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
+
+<!--[if lt IE 9]>
+<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+<![endif]-->
+
+<?php wp_head(); ?>
+</head>
+	
+	
+<header class="half-section-1-2 no-padding-top no-padding-bottom">
+	<div class="left">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-8 col-lg-offset-2 text-center"> 
+					 <a class="brand editable" href="#"><img class="img-responsive img-center" src="<?php echo get_template_directory_uri(); ?>/images/logo_hip.png" alt="logo" /></a>
+					<h1 class="editable">THE DESIGN</h1>
+						<hr />
+					<h3 class="padding-top-100 editable">MAKEUP WEB SITES</h3>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="right">
+		<div class="container-fluid">
+			<div class="row">
+			</div>
+		</div>
+	</div>
+</header>
